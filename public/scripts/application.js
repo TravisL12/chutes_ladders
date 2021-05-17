32c3cb0a0ce950f5f6a750d90b3c57fa61131c5a
Travis Lawrence <travis.lawrence12@gmail.com>
2021-05-16 20:40:17 -0700

    working

--- public/scripts/application.js
+++ public/scripts/application.js
@@ -27,9 +27,76 @@ function spinSpinner() {
   return Math.floor(Math.random() * 6) + 1;
 }
 
-const spinnerButton = document.getElementById('spin');
-const spinResult = document.getElementById('spin-result');
-spinnerButton.addEventListener('click', (event) => {
-  const spinValue = spinSpinner();
-  spinResult.textContent = spinValue;
-});
+class Tile {
+  constructor(i) {
+    this.el = document.createElement('div');
+    this.el.innerHTML = `
+    <span>${i}</span>
+      <div class="players"></div>
+    `;
+    this.el.classList = 'tile';
+    this.players = [];
+    this.playersEl = this.el.querySelector('.players');
+    this.el.id = `tile-${i}`;
+    if (ladders[i]) {
+      this.el.classList.add('ladder');
+    }
+    if (chutes[i]) {
+      this.el.classList.add('chute');
+    }
+  }
+
+  updatePlayers = (playerNumber) => {
+    const number = playerNumber;
+    this.players[number] = !this.players[number];
+    this.playersEl.innerHTML = this.players
+      .map((hasPlayer, i) =>
+        hasPlayer ? `<span class="player-${i}">P${i + 1}</span>` : ''
+      )
+      .join('');
+  };
+}
+
+class Game {
+  constructor() {
+    this.board = document.querySelector('.board');
+    this.players = [{ currentTile: 0 }, { currentTile: 0 }];
+    this.currentPlayerIdx = 0;
+    this.tiles = this.buildTiles();
+
+    this.spinResult = document.getElementById('spin-result');
+    this.spinnerButton = document.getElementById('spin');
+    this.spinnerButton.addEventListener('click', this.takeTurn);
+  }
+
+  buildTiles = (count = 100) => {
+    const tiles = [];
+    for (let i = count; i > 0; i--) {
+      const tile = new Tile(i);
+      tiles.push(tile);
+      this.board.appendChild(tile.el);
+    }
+    return tiles;
+  };
+
+  updateTile = () => {
+    this.tiles[
+      100 - this.players[this.currentPlayerIdx].currentTile
+    ]?.updatePlayers(this.currentPlayerIdx);
+  };
+
+  turnOver = () => {
+    this.currentPlayerIdx = (this.currentPlayerIdx + 1) % this.players.length;
+  };
+
+  takeTurn = () => {
+    const spinValue = spinSpinner();
+    this.spinResult.textContent = spinValue;
+    this.updateTile(); // deactivate current square
+    this.players[this.currentPlayerIdx].currentTile += spinValue;
+    this.updateTile(); // activate new square
+    this.turnOver();
+  };
+}
+
+new Game();
